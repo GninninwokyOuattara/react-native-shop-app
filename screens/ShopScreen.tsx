2bac43f2834ef1e03f0d8cbb8c2d0d7131ee4b0b
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-16 14:03:53 +0000

    loading product logic in case error happen

--- screens/ShopScreen.tsx
+++ screens/ShopScreen.tsx
@@ -1,6 +1,11 @@
-import React, { useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
-import { Item } from "react-navigation-header-buttons";
+import React, { useEffect, useState, useCallback } from "react";
+import {
+    View,
+    Text,
+    ActivityIndicator,
+    RefreshControl,
+    ScrollView,
+} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import ProductList from "../components/ProductList";
 import Product from "../models/product";
@@ -12,14 +17,72 @@ const renderItem: React.FC<{ item: Product }> = ({ item }) => {
 };
 
 const HomeScreen = (props: any) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState<false | string>(false);
+    const [refreshing, setRefreshing] = useState(false);
     const shopProducts = useSelector(
         (state: RootState) => state.products.availableProducts
     );
     const dispatch = useDispatch();
 
+    const load = useCallback(async () => {
+        setIsLoading(true);
+        setIsError(false);
+        try {
+            await dispatch(fetchProduct());
+        } catch (error) {
+            setIsError(error);
+        }
+
+        setIsLoading(false);
+    }, [setIsLoading, setIsError]);
     useEffect(() => {
-        dispatch(fetchProduct());
+        load();
     }, [dispatch]);
+
+    if (isLoading) {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <ActivityIndicator size="large" color="blue" />
+            </View>
+        );
+    }
+
+    if (!isLoading && isError) {
+        return (
+            <ScrollView
+                contentContainerStyle={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <RefreshControl refreshing={refreshing} onRefresh={load} />
+
+                <Text>{isError}</Text>
+            </ScrollView>
+        );
+    }
+
+    if (!isLoading && !shopProducts.length) {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <Text>No Product Found</Text>
+            </View>
+        );
+    }
 
     return (
         <View
