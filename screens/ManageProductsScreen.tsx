4b1ce2cf7cc2cfbe3509ffa03cd4a14d1324670b
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2021-08-16 15:43:15 +0000

    screen load data everytime it is navigated to

--- screens/ManageProductsScreen.tsx
+++ screens/ManageProductsScreen.tsx
@@ -1,13 +1,51 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import { useSelector } from "react-redux";
 import { RootState } from "../types";
 import ProductList from "../components/ProductList";
+import { ScreenNavigationProps, ManageProductsStack } from "../types";
+import useLoad from "../hooks/useLoad";
 
-const ManageProductsScreen = (props: any) => {
+const ManageProductsScreen: React.FC<
+    ScreenNavigationProps<ManageProductsStack>
+> = (props) => {
+    const { isError, isLoading, load } = useLoad();
     const userProducts = useSelector(
         (state: RootState) => state.products.userProducts
     );
+
+    useEffect(() => {
+        const unsubFocus = props.navigation.addListener("focus", load);
+        return unsubFocus;
+    }, [load]);
+
+    if (isLoading) {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <ActivityIndicator size="large" color="blue" />
+            </View>
+        );
+    }
+
+    if (isError) {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <Text>{isError}</Text>
+            </View>
+        );
+    }
 
     if (!userProducts) {
         return (
